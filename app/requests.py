b4fae7eb0d6e397b1a70789bef0d5e12ca5bce72
leah <leahgakii74@gmail.com>
2019-04-15 16:08:17 +0300

    creates a function that takes in a list of dictionaries

--- app/requests.py
+++ app/requests.py
@@ -10,11 +10,11 @@ base_url_source=None
 
 base_url_articles=None
 
-# def configure_request(app):
-#     global api_key,base_url_source,base_url_articles
-#     api_key = app.config['NEWS_API_KEY']
-#     base_url_source=app.config['BASE_URL_SOURCES']
-#     base_url_articles=app.config['BASE_URL_ARTICLES']
+def configure_request(app):
+    global api_key,base_url_source,base_url_articles
+    api_key = app.config['NEWS_API_KEY']
+    base_url_source=app.config['BASE_URL_SOURCES']
+    base_url_articles=app.config['BASE_URL_ARTICLES']
 
 def get_source():
     '''
@@ -36,34 +36,34 @@ def get_source():
     return source_results
 
 
-def process_results(source_list):
+# def process_results(source_list):
 
-    source_results = []
-    for source_item in source_list:
-        id = source_item.get('id')
-        name = source_item.get('name')
-        description = source_item.get('description')
-        source_object = Source(id,name,description)
-        source_results.append(source_object)
-    return source_results
-def get_articles(id):
-    '''
-    Function that gets the json response to our url request
-    '''
+#     source_results = []
+#     for source_item in source_list:
+#         id = source_item.get('id')
+#         name = source_item.get('name')
+#         description = source_item.get('description')
+#         source_object = Source(id,name,description)
+#         source_results.append(source_object)
+#     return source_results
+# def get_articles(id):
+#     '''
+#     Function that gets the json response to our url request
+#     '''
 
  
-    get_articles_url = base_url_articles.format(id,api_key)
-    with urllib.request.urlopen(get_articles_url) as url:
-        get_articles_data = url.read()
-        get_articles_response = json.loads(get_articles_data)
+#     get_articles_url = base_url_articles.format(id,api_key)
+#     with urllib.request.urlopen(get_articles_url) as url:
+#         get_articles_data = url.read()
+#         get_articles_response = json.loads(get_articles_data)
 
-        articles_results = None
+#         articles_results = None
 
-        if get_articles_response['articles']:
-            articles_results_list = get_articles_response['articles']
-            articles_results = process_article_results(articles_results_list)
+#         if get_articles_response['articles']:
+#             articles_results_list = get_articles_response['articles']
+#             articles_results = process_article_results(articles_results_list)
            
-    return articles_results
+#     return articles_results
 
 
 def process_article_results(articles_list):
