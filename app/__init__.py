1a0260de6e4d1cefd5360d3cee099b791cc53370
leah <leahgakii74@gmail.com>
2019-04-15 16:21:46 +0300

    creation of button

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,17 @@
-# from flask import Flask
+from flask import Flask
 
-# from flask_bootstrap import Bootstrap
-# from config import config_options
-# bootstrap = Bootstrap()
-# def create_app(config_name):
-#     app = Flask(__name__)
-#     app.config.from_object(config_options[config_name])
+from flask_bootstrap import Bootstrap
+from config import config_options
+bootstrap = Bootstrap()
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config_options[config_name])
     
-#     from .main import main as main_blueprint
-#     app.register_blueprint(main_blueprint)
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     
-#     bootstrap.init_app(app)
+    bootstrap.init_app(app)
 
     
     from .requests import configure_request
